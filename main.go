d820e2381884d7ea5f01c54cc621214ba5d4a59e
Jedikrigeren <59309403+Jedikrigeren@users.noreply.github.com>
2023-11-17 14:41:01 +0100

    removed the cron scheduler from the script

--- main.go
+++ main.go
@@ -5,8 +5,6 @@ import (
 	"illum_sales_file_loader/sap_api_wrapper"
 	"illum_sales_file_loader/utils"
 	"time"
-
-	"github.com/go-co-op/gocron"
 )
 
 func main() {
@@ -20,20 +18,4 @@ func main() {
 	sap_api_wrapper.SapApiPostLogout()
 
 	fmt.Printf("%v: Success \n", time.Now().UTC().Format("2006-01-02 15:04:05"))
-	fmt.Printf("%v: Started the Cron Scheduler", time.Now().UTC().Format("2006-01-02 15:04:05"))
-
-	s := gocron.NewScheduler(time.UTC)
-	_, _ = s.Cron("0 * * * *").SingletonMode().Do(func() {
-		fmt.Printf("%v: Started the Script\n", time.Now().Format("2006-01-02 15:04:05"))
-
-		err := utils.CreateOrdersSap()
-		if err != nil {
-			utils.SendUnknownErrorToTeams(err)
-		}
-
-		sap_api_wrapper.SapApiPostLogout()
-		fmt.Printf("%v: Success \n", time.Now().Format("2006-01-02 15:04:05"))
-	})
-
-	s.StartBlocking()
 }
